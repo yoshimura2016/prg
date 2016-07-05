9acfbdabcb2615d9396dee49de07765601a62a79
Hiroyuki Yoshimura <h-yoshimura@kuf.biglobe.ne.jp>
2016-07-05 13:17:59 +0900

    First Commit

---
+++ prg20.rb
@@ -0,0 +1,19 @@
+﻿class Mitaka
+  def method1
+    c = 1
+    @c = 2
+    @c += c
+    puts @c
+  end
+  
+  def method2
+    c = 2
+    @c += c
+    puts c
+  end
+end
+
+z = Mitaka.new
+z.method1
+z.method2
+
